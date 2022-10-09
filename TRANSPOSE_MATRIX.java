b49bb1246272619490007dd9f81f332f4143017d
parulpundhir1 <106185698+parulpundhir1@users.noreply.github.com>
2022-10-09 14:24:27 +0530

    Add files via upload

---
+++ TRANSPOSE_MATRIX.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TRANSPOSE_MATRIX {
+        public int[][] transpose(int[][] A) {
+            int[][] ans = new int[A[0].length][A.length];
+
+            for (int i = 0; i < A.length; ++i)
+                for (int j = 0; j < A[0].length; ++j)
+                    ans[j][i] = A[i][j];
+
+            return ans;
+        }
+    }
+
